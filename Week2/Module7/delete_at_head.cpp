489f6d3f853623264a4dc96470580e696a2b126b
neloy25 <sadmansafinneloy678@gmail.com>
2025-07-03 21:13:17 +0600

    Delete at head using function

--- Week2/Module7/delete_at_head.cpp
+++ Week2/Module7/delete_at_head.cpp
@@ -1,33 +1,60 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
+class Node
+{
 public:
     int val;
-    Node* next;
-    Node(int val){
+    Node *next;
+
+    Node(int val)
+    {
         this->val = val;
         this->next = NULL;
     }
 };
 
-
-int main()
+void insert_at_tail(Node * &head, Node * &tail,int val)
 {
-    Node* head = new Node(10);
-    Node* a = new Node(20);
-    Node* b = new Node(30);
-
-    head->next = a;
-    a->next = b;
-
-
-    Node * temp = head;
-    while(temp != NULL){
-        cout << temp->val << endl;
-        temp = temp->next;
+    Node *newnode = new Node(val);
+    if(head == NULL){
+        head = newnode;
+        tail = newnode;
+        return;
     }
-    return 0;
+    tail->next = newnode;
+    tail = newnode;
+}
+
+void delete_at_head(Node * &head){
+    Node * delnode = head;
+    head = head->next;
+    delete delnode;
 
 }
 
+void print_list(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->val << endl;
+        head = head->next;
+    }
+}
+
+int main()
+{
+    Node * head = NULL;
+    Node * tail = NULL;
+    int val;
+    while(true){
+        cin >> val;
+        if(val == -1){
+            break;
+        }
+        insert_at_tail(head, tail,val);
+    }
+    delete_at_head(head);
+    print_list(head);
+    return 0;
+}
