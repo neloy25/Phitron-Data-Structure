e9826bebbc3d3ffbcbd900c8be2017bc0b2c0906
neloy25 <sadmansafinneloy678@gmail.com>
2025-07-03 22:01:57 +0600

    delete at tail

--- Week2/Module7/delete_at_tail.cpp
+++ Week2/Module7/delete_at_tail.cpp
@@ -1,41 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
-        int val;
-        Node* next;
-        
-    Node(int val){
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
         this->val = val;
         this->next = NULL;
     }
 };
 
-int main()
+void insert_at_tail(Node * &head, Node * &tail,int val)
 {
-    Node * head = new Node(10);
-    Node * a = new Node(15);
-    Node * b = new Node(20);
-    Node * c = new Node(25);
-
-    head->next = a;
-    a->next = b;
-    b->next = c;
-
-    Node * temp = head;
-    while(temp->next->next != NULL){
-        // cout << temp->val << endl;
-        temp = temp->next;
+    Node *newnode = new Node(val);
+    if(head == NULL){
+        head = newnode;
+        tail = newnode;
+        return;
     }
-    temp->next = NULL;
-
-    temp = head;
-    while(temp != NULL){
-        cout << temp->val << endl;
-        temp = temp->next;
-    }
-    return 0;
-
+    tail->next = newnode;
+    tail = newnode;
 }
 
+void delete_at_any_pos(Node * head, Node * &tail, int idx){
+    while(head->next != tail){
+        head = head->next;
+    }
+    Node * delnode = head->next;
+    tail = head;
+    tail->next = NULL;
+    delete delnode;
+}
+
+void print_list(Node * head)
+{
+    while (head != NULL)
+    {
+        cout << head->val << endl;
+        head = head->next;
+    }
+}
+
+int main()
+{
+    Node * head = NULL;
+    Node * tail = NULL;
+    int val;
+    while(true){
+        cin >> val;
+        if(val == -1){
+            break;
+        }
+        insert_at_tail(head, tail,val);
+    }
+    delete_at_any_pos(head,tail,2);
+    print_list(head);
+    return 0;
+}
