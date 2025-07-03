4953731ee9c77b2bdf89b01ddb5021b94fb93f5a
neloy25 <sadmansafinneloy678@gmail.com>
2025-07-04 00:55:57 +0600

    Insert at head updated bug fixed

--- Week2/Module6/insert_at_head.cpp
+++ Week2/Module6/insert_at_head.cpp
@@ -35,13 +35,13 @@ int main()
     Node * head = new Node(10);
     Node * a = new Node(20);
     Node * b = new Node(30);
-    Node * c = new Node(40);
+    Node * tail = new Node(40);
 
     head->next= a;
     a->next = b;
-    b->next = c;
+    b->next = tail;
 
-    insert_at_head(head,100);
+    insert_at_head(head,tail,100);
     print_list(head);
     return 0;
 
