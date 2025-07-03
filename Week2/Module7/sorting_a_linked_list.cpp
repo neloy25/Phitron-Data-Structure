4037ffe66f391383f4009b1e49c89ffba3666c7b
neloy25 <sadmansafinneloy678@gmail.com>
2025-07-03 22:25:48 +0600

    Sorting a linked list

---
+++ Week2/Module7/sorting_a_linked_list.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node * &head, Node * &tail,int val)
+{
+    Node *newnode = new Node(val);
+    if(head == NULL){
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    tail = newnode;
+}
+
+void sort_linked_list(Node * head){
+    for(Node * i = head; i->next != NULL; i = i->next){
+        for(Node * j = i->next; j != NULL; j = j->next){
+            if(i->val > j->val){
+                swap(i->val,j->val);
+            }
+        }
+    }
+}
+
+void print_list(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->val << endl;
+        head = head->next;
+    }
+}
+
+int main()
+{
+    Node * head = NULL;
+    Node * tail = NULL;
+    int val;
+    while(true){
+        cin >> val;
+        if(val == -1){
+            break;
+        }
+        insert_at_tail(head, tail,val);
+    }
+    sort_linked_list(head);
+    print_list(head);
+    return 0;
+}
