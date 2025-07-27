35647447e1cbc2dd8d2ef4702c866011aa6bd1a3
neloy25 <sadmansafinneloy678@gmail.com>
2025-07-28 02:21:13 +0600

    Formatted the code

--- Week4/Assignment4/Same_or_Not_II.cpp
+++ Week4/Assignment4/Same_or_Not_II.cpp
@@ -1,65 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
     {
-    public:
-        int val;
-        Node *next;
-        Node *prev;
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
 
-        Node(int val)
-        {
-            this->val = val;
-            this->next = NULL;
-            this->prev = NULL;
-        }
-    };
-
-    void insert_at_tail(Node *&head, Node *&tail, int val)
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
     {
-        Node *newnode = new Node(val);
-        if (head == NULL)
-        {
-            head = newnode;
-            tail = newnode;
-            return;
-        }
-
-        newnode->prev = tail;
-        tail->next = newnode;
+        head = newnode;
         tail = newnode;
+        return;
     }
 
-    void delete_at_head(Node *&head, Node *&tail)
+    newnode->prev = tail;
+    tail->next = newnode;
+    tail = newnode;
+}
+
+void delete_at_head(Node *&head, Node *&tail)
+{
+    Node *delnode = head;
+    head = head->next;
+    delete delnode;
+    if (head == NULL)
     {
-        Node *delnode = head;
-        head = head->next;
-        delete delnode;
-        if(head == NULL) {
-            tail = NULL;
-            return;
-        }
-        head->prev = NULL;
+        tail = NULL;
+        return;
     }
+    head->prev = NULL;
+}
 
-    void delete_at_tail(Node *&head, Node *&tail)
+void delete_at_tail(Node *&head, Node *&tail)
+{
+    Node *delnode = tail;
+    if (head == tail)
     {
-        Node *delnode = tail;
-        if (head == tail)
-        {
-            head = NULL;
-            tail = NULL;
-            delete delnode;
-            return;
-        }
-        tail = tail->prev;
-        tail->next = NULL;
+        head = NULL;
+        tail = NULL;
         delete delnode;
+        return;
     }
+    tail = tail->prev;
+    tail->next = NULL;
+    delete delnode;
+}
 
-    Node *head = NULL;
-    Node *tail = NULL;
+Node *head = NULL;
+Node *tail = NULL;
 
 class myQueue
 {
@@ -130,30 +131,37 @@ public:
     }
 };
 
-
 int main()
 {
     myStack st;
     myQueue q;
 
-    int n,m; cin >> n >> m;
-    if(n != m) {
+    int n, m;
+    cin >> n >> m;
+    if (n != m)
+    {
         cout << "NO";
         return 0;
     }
 
-    for(int i = 0; i < n; i++) {
-        int val; cin >> val;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
         st.push(val);
     }
 
-    for(int i = 0; i < m; i++) {
-        int val; cin >> val;
+    for (int i = 0; i < m; i++)
+    {
+        int val;
+        cin >> val;
         q.push(val);
     }
 
-    for(int i = 0; i < n; i++) {
-        if(st.top() != q.front()) {
+    for (int i = 0; i < n; i++)
+    {
+        if (st.top() != q.front())
+        {
             cout << "NO";
             return 0;
         }
@@ -163,4 +171,3 @@ int main()
     cout << "YES";
     return 0;
 }
-
